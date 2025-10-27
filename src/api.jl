c461ef7e68ead5b2dbe192686e0931605cdbcbcb
jake-w-liu <akjake616@gmail.com>
2025-10-27 18:27:37 +0800

    remove ref_size

--- src/api.jl
+++ src/api.jl
@@ -197,11 +197,10 @@ ptn_holo(
     xlabel::String = "",
     ylabel::String = "",
     zrange::Vector{<:Real} = [0, 0],
-    ref_size = 500,
     colorscale = "Jet",
 )
 
-Plots a holographic (heatmap) radiation pattern. Currently I have issues in setting both the axis ratio and the range of the heatmap plot. In order to have an 1:1 aspect ratio, I have tried to fine tune the width and height of the plot. One can try to adjust the `ref_size` keyword to set the figure size. I hope that more improvements can be made in the future.
+Plots a holographic (heatmap) radiation pattern. Currently I have issues in setting both the axis ratio and the range of the heatmap plot. In order to have an 1:1 aspect ratio, I have tried to fine tune the width and height of the plot. 
 
 #### Arguments
 
@@ -209,7 +208,6 @@ Plots a holographic (heatmap) radiation pattern. Currently I have issues in sett
 - `xlabel`: Label for the x-axis (default: `""`)
 - `ylabel`: Label for the y-axis (default: `""`)
 - `zrange`: Range for the z-axis (default: `[0, 0]`)
-- `ref_size`: ref size of the plot in pixels (default: `500`)
 - `colorscale`: Color scale for the heatmap (default: `"Jet"`)
 """
 function ptn_holo(
@@ -217,7 +215,6 @@ function ptn_holo(
     xlabel::String = "",
     ylabel::String = "",
     zrange::Vector{<:Real} = [0, 0],
-    ref_size = 500,
     colorscale = "Jet",
 )
     return plot_heatmap(
@@ -227,7 +224,6 @@ function ptn_holo(
         xlabel,
         ylabel,
         zrange,
-        ref_size,
         colorscale,
     )
 end
